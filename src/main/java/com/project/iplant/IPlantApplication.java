77d4d6d6e692f682aabce596f9c5f5897e7baaba
Karolina Petrova <karolinapetrovawork@gmail.com>
2020-03-28 01:03:06 +0200

    Added base for Application

---
+++ src/main/java/com/project/iplant/IPlantApplication.java
@@ -0,0 +1,13 @@
+package com.project.iplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IPlantApplication.class, args);
+	}
+
+}
